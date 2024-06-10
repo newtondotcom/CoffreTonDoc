fa8c28192bda49bfb25090f81c9651e9af112469
newtondotcom <asphalt9fr@gmail.com>
2024-06-10 16:50:28 +0200

    regenerate project with nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: [],
   shadcn: {
     /**
      * Prefix for all the imported component
