c96d946497525aeab0fdff03de770cd71401ffe0
newtondotcom <asphalt9fr@gmail.com>
2024-06-12 22:39:01 +0200

    nuxt ui install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: [],
+  modules: ["@nuxt/ui"],
   shadcn: {
     /**
      * Prefix for all the imported component
