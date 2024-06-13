d7530dc2192250f3610adf15877dc6374a8e2582
newtondotcom <asphalt9fr@gmail.com>
2024-06-13 17:18:31 +0200

    working breadcrumb component

---
+++ components/layout/breadcrumb.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex flex-row justify-between items-center px-4">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink @click="navigateToRoot">
+              Home
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator v-if="breadcrumb.length" />
+          <BreadcrumbItem v-for="(crumb, index) in breadcrumb" :key="index">
+            <BreadcrumbLink @click="navigateToFolder(crumb.id)">
+              {{ crumb.name }}
+            </BreadcrumbLink>
+            <BreadcrumbSeparator v-if="index < breadcrumb.length - 1" />
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+  } from '@/components/ui/breadcrumb';
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from '@/components/ui/dropdown-menu';
+  import { defineProps } from 'vue';
+  
+  defineProps({
+    navigateToFolder: Function,
+    navigateToRoot : Function,
+    breadcrumb : Array
+  });
+  </script>
+  
