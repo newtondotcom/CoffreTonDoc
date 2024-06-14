6eebca442dc3dec528565ccb5d24d54dfa83228a
newtondotcom <asphalt9fr@gmail.com>
2024-06-14 18:38:40 +0200

    try to include a folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        "light-background": 'var(--light-background)',
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
