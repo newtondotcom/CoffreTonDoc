0a007f5d70793a5ef0c64999ee28613f4ac70eac
newtondotcom <asphalt9fr@gmail.com>
2024-06-14 17:31:36 +0200

    composant access fichier

---
+++ components/icons/statut.vue
@@ -0,0 +1,41 @@
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child>
+    <span class="w-4 h-4 text-center">
+        <User v-if="statut === 'self'"/>
+        <Users  v-if="statut === 'shared'"/>
+        <Building  v-if="statut =='work'"/>
+    </span>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>
+      {{
+        statut === "self"
+          ? `Vous`
+          : statut === "shared"
+          ? `Partagé`
+          : statut === "work"
+          ? `Entreprise`
+          : ''
+      }}
+      </p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    props: {
+      statut: {
+        type: String,
+        required: true
+      }
+    }
+  };
+  </script>
+
+  <script setup lang="ts">
+import { User, Users, Building } from 'lucide-vue-next';
+</script>
