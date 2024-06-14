72b87e326ef0ef3b4456c40338b74226ae4628a4
newtondotcom <asphalt9fr@gmail.com>
2024-06-14 19:27:24 +0200

    dialogue qui marche apres une lutte acharnée

--- components/layout/entity.vue
+++ components/layout/entity.vue
@@ -1,172 +1,182 @@
 <template>
-  <ContextMenu>
-    <ContextMenuTrigger class="w-full flex flex-row justify-between">
-      <IconsDate :date="file.date" />
-      <div class="flex flex-row w-[60%] items-center">
-        <div class="relative w-16 shrink-0">
-          <IconsFolder v-if="file.isFolder" />
-          <IconsThumbnail :extension="file.extension" v-else />
-        </div>
-        <div class="flex flex-col pl-2 min-w-0">
-          <div class="mb-0.5 block overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold lg:pr-0 pr-4 hover:underline cursor-text">
-            {{ file.name }}
-          </div>
-        </div>
-      </div>
-      <IconsSize :size="file.size" />
-      <IconsStatut :statut="file.statut" />
-    </ContextMenuTrigger>
-    <ContextMenuContent class="w-64 bg-white">
-      <ContextMenuItem @click="openItem(file)">
-        Open
-        <ContextMenuShortcut>Enter</ContextMenuShortcut>
-      </ContextMenuItem>
-
-      <Dialog>
-        <DialogTrigger as-child>
-          <ContextMenuItem>
-            Rename
-            <ContextMenuShortcut>F2</ContextMenuShortcut>
-          </ContextMenuItem>
-        </DialogTrigger>
-        <DialogContent class="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Rename File</DialogTitle>
-            <DialogDescription>
-              Enter a new name for the file.
-            </DialogDescription>
-          </DialogHeader>
-          <div class="grid gap-4 py-4">
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label for="newName" class="text-right">
-                New Name
-              </Label>
-              <Input id="newName" v-model="newName" class="col-span-3" />
+<Dialog>
+    <ContextMenu>
+        <ContextMenuTrigger class="w-full flex flex-row justify-between">
+            <IconsDate :date="file.date" />
+            <div class="flex flex-row w-[60%] items-center">
+                <div class="relative w-16 shrink-0">
+                    <IconsFolder v-if="file.isFolder" />
+                    <IconsThumbnail :extension="file.extension" v-else />
+                </div>
+                <div class="flex flex-col pl-2 min-w-0">
+                    <div class="mb-0.5 block overflow-hidden text-ellipsis whitespace-nowrap text-sm font-bold lg:pr-0 pr-4 hover:underline cursor-text">
+                        {{ file.name }}
+                    </div>
+                </div>
             </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" @click="renameFile(file, newName)">
-              Save Changes
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+            <IconsSize :size="file.size" />
+            <IconsStatut :statut="file.statut" />
+        </ContextMenuTrigger>
+        <ContextMenuContent class="w-64 bg-white">
+            <ContextMenuItem @click="setState('open')">
+                Open
+                <ContextMenuShortcut>Enter</ContextMenuShortcut>
+            </ContextMenuItem>
 
-      <Dialog>
-        <DialogTrigger as-child>
-          <ContextMenuItem>
-            Delete
-            <ContextMenuShortcut>Del</ContextMenuShortcut>
-          </ContextMenuItem>
-        </DialogTrigger>
-        <DialogContent class="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Delete File</DialogTitle>
-            <DialogDescription>
-              Are you sure you want to delete this file? This action cannot be undone.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Button type="submit" @click="deleteItem(file)">
-              Delete
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+            <ContextMenuItem @click="setState('rename')">
+                Rename
+                <ContextMenuShortcut>F2</ContextMenuShortcut>
+            </ContextMenuItem>
 
-      <Dialog>
-        <DialogTrigger as-child>
-          <ContextMenuItem>
-            Access
-            <ContextMenuShortcut>Shift + A</ContextMenuShortcut>
-          </ContextMenuItem>
-        </DialogTrigger>
-        <DialogContent class="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Change Access</DialogTitle>
-            <DialogDescription>
-              Modify the access permissions for this file.
-            </DialogDescription>
-          </DialogHeader>
-          <div class="grid gap-4 py-4">
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label for="accessLevel" class="text-right">
-                Access Level
-              </Label>
-              <Input id="accessLevel" v-model="accessLevel" class="col-span-3" />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" @click="changeAccess(file, accessLevel)">
-              Save Changes
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
 
-      <ContextMenuSeparator v-if="file.isFolder" />
+      <DialogTrigger asChild>
+            <ContextMenuItem @click="setState('delete')">
+                Delete
+                <ContextMenuShortcut>Del</ContextMenuShortcut>
+            </ContextMenuItem>
+      </DialogTrigger>
 
-      <Dialog v-if="file.isFolder">
-        <DialogTrigger as-child>
-          <ContextMenuItem>
-            New File Inside
-            <ContextMenuShortcut>Ctrl + N</ContextMenuShortcut>
-          </ContextMenuItem>
-        </DialogTrigger>
-        <DialogContent class="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>New File</DialogTitle>
-            <DialogDescription>
-              Enter the name for the new file.
-            </DialogDescription>
-          </DialogHeader>
-          <div class="grid gap-4 py-4">
-            <div class="grid grid-cols-4 items-center gap-4">
-              <Label for="newFileName" class="text-right">
-                File Name
-              </Label>
-              <Input id="newFileName" v-model="newFileName" class="col-span-3" />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" @click="createNewFile(file, 'file', newFileName)">
-              Create
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+      <DialogTrigger asChild>
+            <ContextMenuItem @click="setState('access')">
+                Access
+                <ContextMenuShortcut>Shift + A</ContextMenuShortcut>
+            </ContextMenuItem>
+      
+      </DialogTrigger>
 
-      <Dialog v-if="file.isFolder">
-        <DialogTrigger as-child>
-          <ContextMenuItem>
-            New Folder Inside
-            <ContextMenuShortcut>Ctrl + N</ContextMenuShortcut>
-          </ContextMenuItem>
-        </DialogTrigger>
-        <DialogContent class="sm:max-w-[425px]">
-          <DialogHeader>
+            <ContextMenuSeparator v-if="file.isFolder" />
+
+
+      <DialogTrigger v-if="file.isFolder" asChild>
+            <ContextMenuItem @click="setState('newFile')">
+                New File Inside
+                <ContextMenuShortcut>Ctrl + N</ContextMenuShortcut>
+            </ContextMenuItem>
+      </DialogTrigger>
+
+
+      <DialogTrigger v-if="file.isFolder" asChild>
+            <ContextMenuItem @click="setState('newFolder')">
+                New Folder Inside
+                <ContextMenuShortcut>Ctrl + N</ContextMenuShortcut>
+            </ContextMenuItem>
+      </DialogTrigger>
+        </ContextMenuContent>
+
+    </ContextMenu>
+
+    <DialogContent v-if='stateDialog=="newFile"' class="sm:max-w-[425px]">
+        <DialogHeader>
             <DialogTitle>New Folder</DialogTitle>
             <DialogDescription>
-              Enter the name for the new folder.
+                Enter the name for the new folder.
             </DialogDescription>
-          </DialogHeader>
-          <div class="grid gap-4 py-4">
+        </DialogHeader>
+        <div class="grid gap-4 py-4">
             <div class="grid grid-cols-4 items-center gap-4">
-              <Label for="newFolderName" class="text-right">
-                Folder Name
-              </Label>
-              <Input id="newFolderName" v-model="newFolderName" class="col-span-3" />
+                <Label for="newFolderName" class="text-right">
+                    Folder Name
+                </Label>
+                <Input id="newFolderName" v-model="newFolderName" class="col-span-3" />
             </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" @click="createNewFolder(file, 'folder', newFolderName)">
-              Create
+        </div>
+        <DialogFooter>
+        <DialogClose as-child>
+            <Button type="submit" @click.prevent="createNewFolder(file, 'folder', newFolderName)">
+                Create
             </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </ContextMenuContent>
-  </ContextMenu>
+          </DialogClose>
+        </DialogFooter>
+    </DialogContent>
+
+    <DialogContent v-if="stateDialog === 'newFolder'" class="sm:max-w-[425px]">
+        <DialogHeader>
+            <DialogTitle>New File</DialogTitle>
+            <DialogDescription>
+                Enter the name for the new file.
+            </DialogDescription>
+        </DialogHeader>
+        <div class="grid gap-4 py-4">
+            <div class="grid grid-cols-4 items-center gap-4">
+                <Label for="newFileName" class="text-right">
+                    File Name
+                </Label>
+                <Input id="newFileName" v-model="newFileName" class="col-span-3" />
+            </div>
+        </div>
+        <DialogFooter>
+        <DialogClose as-child>
+            <Button type="submit" @click.prevent="createNewFile(file, 'file', newFileName)">
+                Create
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+    </DialogContent>
+
+    <DialogContent v-if="stateDialog === 'rename'" class="sm:max-w-[425px]">
+        <DialogHeader>
+            <DialogTitle>Change Access</DialogTitle>
+            <DialogDescription>
+                Modify the access permissions for this file.
+            </DialogDescription>
+        </DialogHeader>
+        <div class="grid gap-4 py-4">
+            <div class="grid grid-cols-4 items-center gap-4">
+                <Label for="accessLevel" class="text-right">
+                    Access Level
+                </Label>
+                <Input id="accessLevel" v-model="accessLevel" class="col-span-3" />
+            </div>
+        </div>
+        <DialogFooter>
+        <DialogClose as-child>
+            <Button type="submit" @click.prevent="changeAccess(file, accessLevel)">
+                Save Changes
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+    </DialogContent>
+
+    <DialogContent v-if="stateDialog === 'delete'" class="sm:max-w-[425px]">
+        <DialogHeader>
+            <DialogTitle>Delete File</DialogTitle>
+            <DialogDescription>
+                Are you sure you want to delete this file? This action cannot be undone.
+            </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+        <DialogClose as-child>
+            <Button type="submit" @click.prevent="deleteItem(file)">
+                Delete
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+    </DialogContent>
+
+    <DialogContent v-if="stateDialog === 'access'" class="sm:max-w-[425px]">
+        <DialogHeader>
+            <DialogTitle>Rename File</DialogTitle>
+            <DialogDescription>
+                Enter a new name for the file.
+            </DialogDescription>
+        </DialogHeader>
+        <div class="grid gap-4 py-4">
+            <div class="grid grid-cols-4 items-center gap-4">
+                <Label for="newName" class="text-right">
+                    New Name
+                </Label>
+                <Input id="newName" v-model="newName" class="col-span-3" />
+            </div>
+        </div>
+        <DialogFooter>
+        <DialogClose as-child>
+            <Button type="submit" @click.prevent="renameFile(file, newName)">
+                Save Changes
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+    </DialogContent>
+</Dialog>
 </template>
 
 <script setup lang="ts">
@@ -176,11 +186,30 @@ defineProps({
   renameFile: Function,
   deleteItem: Function,
   createNewFile: Function,
-  createNewFolder : Function,
+  createNewFolder: Function,
   changeAccess: Function,
 });
+
 const newName = ref('');
 const newFileName = ref('');
 const newFolderName = ref('');
 const accessLevel = ref('');
+const stateDialog = ref('newFile'); // Default state
+const showDialog = ref(true);
+
+function setState(newState) {
+  stateDialog.value = newState;
+}
+
+watch(() => stateDialog, () => {
+  showDialog.value = true;
+});
+
+function preventDefaultEvent(event: Event) {
+  event.preventDefault();
+}
+
+function test() {
+  console.log('test');
+}
 </script>
