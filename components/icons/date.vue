9b73c7c72806c9afed45974c81ffa614be1d89ea
newtondotcom <asphalt9fr@gmail.com>
2024-06-14 17:31:11 +0200

    composant date

---
+++ components/icons/date.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- Display the formatted date string -->
+    <span>{{ new Date(date).toLocaleString() }}</span>
+  </template>
+  
+  <script setup lang="ts">
+defineProps({
+    date: String
+});
+  </script>
+  
