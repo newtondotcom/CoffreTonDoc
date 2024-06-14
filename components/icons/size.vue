084bf42194b3e200a3023e553b3b879d1e793b3d
newtondotcom <asphalt9fr@gmail.com>
2024-06-14 17:31:21 +0200

    composant taille

---
+++ components/icons/size.vue
@@ -0,0 +1,25 @@
+<template>
+    <span class="h-auto w-20 text-center">
+      {{
+        size < 1000
+          ? `${size} B`
+          : size < 1000000
+          ? `${(size / 1000).toFixed(2)} KB`
+          : size < 1000000000
+          ? `${(size / 1000000).toFixed(2)} MB`
+          : `${(size / 1000000000).toFixed(2)} GB`
+      }}
+    </span>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    props: {
+      size: {
+        type: Number,
+        required: true
+      }
+    }
+  };
+  </script>
+  
