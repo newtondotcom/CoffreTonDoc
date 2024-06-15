15757af017ccb880a170c8b98bd26bbb48db01a1
newtondotcom <asphalt9fr@gmail.com>
2024-06-15 22:03:39 +0200

    file upload endpoint

---
+++ server/api/file/upload.ts
@@ -0,0 +1,17 @@
+import { createFile} from "../../data/files";
+import generateUniqueName from "../../data/s3";
+
+export default defineEventHandler(async (event) => {
+    const user_id = event.context.user_id;
+    const body = await readBody(event);
+    const name =  body.name;
+    const idParent = body.idParent;
+    const statut = body.statut;
+    const size = body.size;
+    const extension = body.extension;
+    const id = await createFile(name, extension, idParent, size, statut, user_id);
+    const uname = generateUniqueName();
+    const urlUpload = await createPresignedUrlDownload(user_id, uname);
+    const idfinal = id.id;
+    return {idfinal,urlUpload};
+})
