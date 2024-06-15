f560f90a637b51193265b4584ceae2ec6cd292fc
newtondotcom <asphalt9fr@gmail.com>
2024-06-15 16:11:08 +0200

    locale select menu

---
+++ components/button/locale.vue
@@ -0,0 +1,22 @@
+<template>
+    <Select>
+    <SelectTrigger class="w-[80px]">
+      <SelectValue placeholder="🌎" />
+    </SelectTrigger>
+    <SelectContent>
+        <SelectItem @click="setLocale('en')" value="🇺🇸">
+          🇺🇸
+        </SelectItem>
+        <SelectItem @click="setLocale('fr')" value="🇲🇫">
+          🇲🇫
+        </SelectItem>
+        <SelectItem @click="setLocale('es')" value="🇪🇦">
+          🇪🇦
+        </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
+
+<script setup lang="ts">
+const { locale, setLocale } = useI18n()
+</script>
