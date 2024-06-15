3e985d737883b091452afee60df32500aa29620a
newtondotcom <asphalt9fr@gmail.com>
2024-06-15 22:49:17 +0200

    layout

--- components/layout/menu.vue
+++ components/layout/menu.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div class="w-[15%] h-full">
+<div class="w-[15%] h-full flex flex-col justify">
 <div class="text-2xl font-extrabold px-6 py-4">VautDoc</div>
 <LayoutLinks />
 <div class="flex flex-row w-full justify-between px-4">
