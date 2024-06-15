b175d1856019994d0b9c0724f11fab86bf562642
newtondotcom <asphalt9fr@gmail.com>
2024-06-15 19:00:44 +0200

    constants file

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+const constants = {
+
+    URL_APP : 'https://app.yogocap.com',
+    URL_LANDING : 'https://yogocap.com',
+    NAME_S3_VIDEOS : 'videos',
+    NAME_S3_UPLOADS : 'uploads',
+    NAME_S3_DOWNLOADS : 'downloads',
+    NAME_S3_THUMBNAILS : 'thumbnails2',
+
+    NB_DAYS_DELETE : 2,
+}
+
+export default constants;
