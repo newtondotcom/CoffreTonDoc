396a2606502c18b2a0ac5b1c643d153dcb24181c
newtondotcom <asphalt9fr@gmail.com>
2024-06-15 16:13:53 +0200

    hi component

---
+++ components/layout/hi.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="grid gap-8 mt-4">
+    <div class="flex flex-row justify-center items-center gap-4">
+        <Avatar class="hidden h-9 w-9 sm:flex">
+            <AvatarImage src="/avatars/01.png" alt="Avatar" />
+            <AvatarFallback>OM</AvatarFallback>
+        </Avatar>
+        <div class="grid gap-1">
+            <p class="text-sm font-medium leading-none">
+                Olivia Martin
+            </p>
+            <p class="text-sm text-muted-foreground">
+                olivia.martin@email.com
+            </p>
+        </div>
+        <!-- 
+              <div class="ml-auto font-medium">
+                +$1,999.00
+              </div>
+                -->
+    </div>
+</div>
+</template>
