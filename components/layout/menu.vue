1d5ec6efa26990208d3b3b86e203b81665e4ae95
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 12:35:37 +0200

    made in france logo

--- components/layout/menu.vue
+++ components/layout/menu.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="w-[15%] h-full flex flex-col justify-between bg-secondary" style="height:100vh">
-    <div class="text-2xl font-extrabold px-6 py-4 text-primary">VautDoc</div>
+    <div class="text-2xl font-extrabold px-6 py-4 text-primary">VautDoc
+    <div class="text-xs text-gray-500 dark:text-white"> {{ $t('made_france')}}</div>
+    </div> 
     <LayoutLinks />
     <div class="flex flex-row w-full justify-between px-4">
         <ButtonColorMode />
