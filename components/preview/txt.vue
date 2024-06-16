ed6b97749b63fd95865eefa67dc0b32e335ca663
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 20:54:03 +0200

    small txt editor

---
+++ components/preview/txt.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+    <textarea
+      v-model="text"
+      class="w-[90vw] h-[90vh] p-4 text-sm dark:bg-dark-background dark:text-white"
+    >
+    </textarea>
+  </div>
+</template>
+
+<script setup lang="ts">
+const text = ref<string>('');
+</script>
+
