83e1e5229439d3f34840db47f2485c4bb2dc9486
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 20:53:25 +0200

    docx reader

---
+++ components/preview/docx.vue
@@ -0,0 +1,30 @@
+<template>
+
+
+  <vue-office-docx
+    :src="docx"
+    style="height: 100vh;"
+    @rendered="renderedHandler"
+    @error="errorHandler"
+  />
+
+</template>
+
+<script setup lang="ts">
+import VueOfficeDocx from '@vue-office/docx';
+import '@vue-office/docx/lib/index.css';
+Vue.component('vue-office-docx', VueOfficeDocx);
+const docx = 'http://static.shanhuxueyuan.com/test6.docx'; // Set the document URL
+
+
+
+function renderedHandler() {
+  console.log('Render completed');
+}
+
+function errorHandler() {
+  console.log('Render failed');
+}
+
+
+</script>
