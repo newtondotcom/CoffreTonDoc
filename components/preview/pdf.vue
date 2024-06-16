49c9b02f94c62abcb69d82dfd17f0eee930561d1
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 20:53:41 +0200

    pdf viewer

---
+++ components/preview/pdf.vue
@@ -0,0 +1,17 @@
+<template>
+
+      <vue-office-pdf 
+        :src="pdf"
+        style="height: 100vh"
+        @rendered="renderedHandler"
+        @error="errorHandler"
+    />
+
+</template>
+
+<script setup lang="ts">
+
+import VueOfficePdf from '@vue-office/pdf'
+const pdf = 'http://static.shanhuxueyuan.com/test.pdf'; // Set the PDF URL
+
+</script>
