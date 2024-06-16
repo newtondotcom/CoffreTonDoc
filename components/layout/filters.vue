935ef04503473e4327bf16bbb73587e12af18400
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 11:44:49 +0200

    j ai oublié ca

--- components/layout/filters.vue
+++ components/layout/filters.vue
@@ -1,28 +1,38 @@
 <template>
-    <div class="flex items-center overflow-x-auto pb-3 pl-4">
-        <Button variant="secondary" class="mx-2">
-            <Search />
-            <span class="ml-2 text-sm font-bold">{{ $t('spotlight') }}</span>
-        </Button>
-        <Button variant="secondary" class="mx-2">
-            <Filter />
-            <span class="ml-2 text-sm font-bold">{{ $t('my_files') }}</span>
-        </Button>
-        <Button variant="secondary" class="mx-2">
-            <Upload />
-            <span class="ml-2 text-sm font-bold">{{ $t('upload_create') }}</span>
-        </Button>
-        <Button variant="secondary" class="mx-2">
-            <SquareCheckBig />
-            <span class="ml-2 text-sm font-bold">{{ $t('select') }}</span>
-        </Button>
-        <Button variant="secondary" class="mx-2">
-            <FolderKanban />
-            <span class="ml-2 text-sm font-bold">{{ $t('view') }}</span>
-        </Button>
-    </div>
+<div class="flex items-center overflow-x-auto pb-3 pl-4">
+    <Button variant="secondary" class="mx-2" @click="toogleSearch">
+        <Search />
+        <span class="ml-2 text-sm font-bold">{{ $t('spotlight') }}</span>
+    </Button>
+    <Button variant="secondary" class="mx-2">
+        <Filter />
+        <span class="ml-2 text-sm font-bold">{{ $t('my_files') }}</span>
+    </Button>
+    <Button variant="secondary" class="mx-2">
+        <Upload />
+        <span class="ml-2 text-sm font-bold">{{ $t('upload_create') }}</span>
+    </Button>
+    <Button variant="secondary" class="mx-2">
+        <SquareCheckBig />
+        <span class="ml-2 text-sm font-bold">{{ $t('select') }}</span>
+    </Button>
+    <Button variant="secondary" class="mx-2">
+        <FolderKanban />
+        <span class="ml-2 text-sm font-bold">{{ $t('view') }}</span>
+    </Button>
+</div>
 </template>
 
-<script setup lang="ts">
-import { Search, Filter, Upload, SquareCheckBig, FolderKanban } from 'lucide-vue-next';
+<script lang="ts">
+import {
+    Search,
+    Filter,
+    Upload,
+    SquareCheckBig,
+    FolderKanban
+} from 'lucide-vue-next';
+defineProps({
+    toogleSearch: Function,
+    isSearching: Boolean
+});
 </script>
