511c5ac8ffa63b36b277f989f57c1924a37d82b1
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 12:35:56 +0200

    format

--- components/icons/statut.vue
+++ components/icons/statut.vue
@@ -2,35 +2,35 @@
   <TooltipProvider>
     <Tooltip>
       <TooltipTrigger as-child>
-    <span class="w-4 h-4 text-center hover:bg-secondary dark:hover:bg-dark-foreground flex justify-center items-center ">
-        <User v-if="statut === 'USER'"/>
-        <Users  v-if="statut === 'SHARED'"/>
-        <Building  v-if="statut =='COMPANY'"/>
-    </span>
+        <span class="w-5 hover:bg-secondary dark:hover:bg-dark-foreground flex justify-center items-center">
+          <User v-if="statut === 'USER'" />
+          <Users v-if="statut === 'SHARED'" />
+          <Building v-if="statut === 'COMPANY'" />
+        </span>
       </TooltipTrigger>
       <TooltipContent>
         <p>
-      {{
-        statut === "USER"
-          ? $t('access_you')
-          : statut === "SHARED"
-          ? $t('access_shared')
-          : statut === "COMPANY"
-          ? $t('access_company')
-          : ''
-      }}
-      </p>
+          {{
+            statut === "USER"
+              ? $t('access_you')
+              : statut === "SHARED"
+              ? $t('access_shared')
+              : statut === "COMPANY"
+              ? $t('access_company')
+              : ''
+          }}
+        </p>
       </TooltipContent>
     </Tooltip>
   </TooltipProvider>
-  </template>
+</template>
 
-  <script setup lang="ts">
+<script setup lang="ts">
 import { User, Users, Building } from 'lucide-vue-next';
-defineProps({      
+defineProps({
   statut: {
-        type: String,
-        required: true
-    },
+    type: String,
+    required: true
+  },
 });
 </script>
