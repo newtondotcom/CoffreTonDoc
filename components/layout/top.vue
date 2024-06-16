2b663dbbcc475ea74a5e0000d5902feb90224ce7
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 11:44:39 +0200

    working command

--- components/layout/top.vue
+++ components/layout/top.vue
@@ -1,13 +1,19 @@
 <template>
-    <!--  lg:hidden -->
+<!--  lg:hidden -->
 <div class="sticky top-0 z-[19] flex w-full items-center justify-between bg-white pb-5 pt-2 px-4 text-center dark:bg-dark-background">
-<div class="relative flex font-bold select-none items-center text-left min-w-0 lg:pr-6 pr-12">
-    {{ $t('my_files') }}
+    <div class="relative flex font-bold select-none items-center text-left min-w-0 lg:pr-6 pr-12">
+        {{ $t('my_files') }}
+    </div>
+    <LayoutFilters :toogleSearch />
 </div>
-<LayoutFilters />
-</div>
+
+<ButtonCommand :toogleSearch :isSearching />
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+const isSearching = ref(false);
 
+function toogleSearch() {
+    isSearching.value = !isSearching.value;
+}
 </script>
