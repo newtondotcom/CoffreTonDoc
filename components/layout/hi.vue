05cc652b3fdf96e7e5920a71c3726fc7bedc0f92
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 12:36:05 +0200

    print based on cookie

--- components/layout/hi.vue
+++ components/layout/hi.vue
@@ -3,14 +3,14 @@
     <div class="flex flex-row justify-center items-center gap-4">
         <Avatar class="hidden h-9 w-9 sm:flex">
             <AvatarImage src="/avatars/01.png" alt="Avatar" />
-            <AvatarFallback>OM</AvatarFallback>
+            <AvatarFallback>{{initials}}</AvatarFallback>
         </Avatar>
         <div class="grid gap-1">
             <p class="text-sm font-medium leading-none">
                 Olivia Martin
             </p>
             <p class="text-sm text-muted-foreground">
-                olivia.martin@email.com
+                {{email}}
             </p>
         </div>
         <!-- 
@@ -24,3 +24,25 @@
     </Button>
 </div>
 </template>
+
+<script setup lang="ts">
+
+const email_cookie = useCookie('email');
+if (!email_cookie.value) {
+    email_cookie.value = 'asphalt9fr@gmail.com';
+}
+const email = ref(email_cookie.value);
+
+const name_cookie = useCookie('name');
+if (!name_cookie.value) {
+    name_cookie.value = 'Newton IsOp';
+}
+const name = ref(name_cookie.value);
+
+const initials_cookie = useCookie('initials');
+if (!initials_cookie.value) {
+    initials_cookie.value = 'NI';
+}
+const initials = ref(initials_cookie.value);
+
+</script>
