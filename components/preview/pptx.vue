566b3646171680e28f68e548d25efcc931eb38ce
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 20:53:52 +0200

    try : pptx viewer

---
+++ components/preview/pptx.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const url = 'https://scholar.harvard.edu/files/torman_personal/files/samplepptx.pptx';
+
+// convert to pdf and display pdf
+
+
+
+</script>
+
+<style scoped>
+.pb-3\/4 {
+  padding-bottom: 75%;
+}
+</style>
