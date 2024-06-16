ddfb8e690bc828348fbf689ca3b51a8f9f782a5d
newtondotcom <asphalt9fr@gmail.com>
2024-06-16 12:36:16 +0200

    reduced to useful links

--- components/layout/links.vue
+++ components/layout/links.vue
@@ -1,29 +1,17 @@
 <template>
-<div class="flex-1 border-primary border-2 rounded-xl mx-2 my-4">
+<div class="flex-1 mx-2 my-4">
     <div class="grid items-start px-2 text-sm font-medium lg:px-1">
-        <a href="/" class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+        <NuxtLink to="/platform" class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
             <Home class="h-4 w-4" />
-            Dashboard
-        </a>
-        <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
-            <ShoppingCart class="h-4 w-4" />
-            Orders
-            <Badge class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+            {{ $t('links_my_files')}}
+        </NuxtLink>
+        <NuxtLink to="/account" class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+            <Settings class="h-4 w-4" />
+            {{ $t('links_my_account')}}
+            <Badge class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full text-secondary">
                 6
             </Badge>
-        </a>
-        <a href="#" class="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
-            <Package class="h-4 w-4" />
-            Products
-        </a>
-        <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
-            <Users class="h-4 w-4" />
-            Customers
-        </a>
-        <a href="#" class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
-            <LineChart class="h-4 w-4" />
-            Analytics
-        </a>
+        </NuxtLink>
     </div>
 </div>
 </template>
@@ -31,9 +19,6 @@
 <script setup lang="ts">
 import {
     Home,
-    ShoppingCart,
-    Package,
-    Users,
-    LineChart
+    Settings
 } from 'lucide-vue-next';
 </script>
