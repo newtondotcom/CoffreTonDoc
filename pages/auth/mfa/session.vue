d84f2aa1cc70c7e914782685cc0b7c7683e3902e
newtondotcom <asphalt9fr@gmail.com>
2024-06-21 19:11:18 +0200

    new structure

---
+++ pages/auth/mfa/session.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const authMFA = ref(false);
+const enrolled = ref(false);
+const loading = ref(true);
+
+onMounted(async () => {
+  try {
+    const { data, error } = await supabase.auth.mfa.getAuthenticatorAssuranceLevel();
+    console.log(data.nextLevel);
+    console.log(data.currentLevel);
+    if (!(data.nextLevel === "aal2" && data.nextLevel !== data.currentLevel)) {
+      authMFA.value = true;
+      enrolled.value = true;
+      //navigateTo("/platform");
+    }
+
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+  }
+});
+</script>
+
+<template>
+<template>
+  <div
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm"
+  >
+      <div v-if="loading">
+        <div class="ml-1 flex">
+          <svg
+            class="animate-spin h-4 w-4 m-1"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+            ></path>
+          </svg>
+      </div>
+      <div>
+      <MfaEnrollment v-if="enrolled && !authMFA"/>
+      <MfaChallenge v-else />
+      </div>
+
+
+  </div>
+</template>
+</template>
