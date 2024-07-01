7b77ac906538d9d265dbecc281418a5b59e627f4
newtondotcom <asphalt9fr@gmail.com>
2024-07-01 23:59:09 +0200

    database and credentials auth are not compatible

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,15 +1,17 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import { NuxtAuthHandler } from "#auth";
 import bcrypt from "bcryptjs";
-import prisma from "../../data/prisma";
+import prisma from "~/server/data/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 const config = useRuntimeConfig();
 export default NuxtAuthHandler({
+  adaptater: PrismaAdapter(prisma),
   secret: config.AUTH_SECRET,
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 Days
+  session: {    
+  //strategy: "database", // Store sessions in the database and store a sessionToken in the cookie for lookups
+  jwt: true,
+  maxAge: 30 * 24 * 60 * 60, // 30 Days
   },
   pages: {
     signIn: "/auth/login",
@@ -39,6 +41,7 @@ export default NuxtAuthHandler({
       },
 
       async authorize(credentials: any) {
+        console.log("credentials", credentials);
         const user = await prisma.user.findUnique({
           where: { email: credentials?.username },
         });
@@ -76,11 +79,12 @@ export default NuxtAuthHandler({
       return token;
     },
     async session({ session, token }: { session: any; token: any }) {
+      // console.log("session", session);
+      // console.log("token", token);
       session.user.id = token.id;
-      session.user.nombre = token.name;
+      session.user.name = token.name;
       session.user.email = token.email;
       return session;
     },
   },
-  adaptater: PrismaAdapter(prisma),
 });
