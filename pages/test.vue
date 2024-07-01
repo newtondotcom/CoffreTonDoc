d54a2949a1a66c3166107f3e16a05e5e99643d76
newtondotcom <asphalt9fr@gmail.com>
2024-07-01 22:01:01 +0200

    test page for sidebase auth

---
+++ pages/test.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+definePageMeta({
+    layout : false,
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: '/',
+  }
+})
+const { status, data, signIn, signOut, refresh   } = useAuth()
+
+</script>
+
+
+<template>
+
+{{ status}}
+{{ data}}
+
+</template>
