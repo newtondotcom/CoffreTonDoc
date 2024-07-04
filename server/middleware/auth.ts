2b176b84997494303ad9edc752cea6f504122425
newtondotcom <asphalt9fr@gmail.com>
2024-07-04 10:17:12 +0200

    don't throw 403 when user not logged in

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,9 +2,10 @@ import { getServerSession } from "#auth";
 
 export default eventHandler(async (event) => {
   const session = await getServerSession(event);
-  if (!session && !event.node.req.url?.startsWith("/api/auth/")) {
+  const allowedRoutes : String[] = [ ""]
+  if (!event.node.req.url?.startsWith("auth/") || !allowedRoutes.includes(!event.node.req.url)) {
     throw { statusMessage: "Unauthenticated", statusCode: 403 };
-  } else {
+  } else if (session) {
     event.context.session = session;
   }
 });
