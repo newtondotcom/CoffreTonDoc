8b39747908d6ddfa9314b422c1dae3f6d0ae945c
newtondotcom <asphalt9fr@gmail.com>
2024-07-05 16:25:30 +0200

    copy button + loading button + cookies management

--- components/auth/register/seed.vue
+++ components/auth/register/seed.vue
@@ -12,9 +12,10 @@
               required
               v-model="input"
             />
-            <Button variant="secondary" @click="copy">
-              Copy
-            </Button>
+            <Button variant="secondary" type="submit" size="sm" class="px-3" @click="copy">
+              <span class="sr-only">Copy</span>
+              <Copy class="w-4 h-4" />
+        </Button>
           </div>
           </div>
           <div class="grid gap-2">
@@ -50,30 +51,8 @@
     </SelectContent>
   </Select>
           </div>
-          <Button type="submit" class="w-full" @click="proceedFinal">
-            <div
-              v-if="loading"
-              aria-label="Loading..."
-              role="status"
-              class="align-middle justify-center flex"
-            >
-              <svg
-                class="animate-spin w-6 h-6 fill-slate-800"
-                viewBox="3 3 18 18"
-              >
-                <path
-                  class="opacity-20"
-                  d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
-                ></path>
-                <path
-                  d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"
-                ></path>
-              </svg>
-            </div>
-            <div v-else>
-              {{ $t("end_configuration") }}
-            </div>
-          </Button>
+          
+          <ButtonLoading :loading text='t("end_configuration")' execute="proceedFinal"/>
         </div>
         <div class="mt-4 text-center text-sm">
           {{ $t("already_account") }}
@@ -87,12 +66,13 @@
 <script setup lang="ts">
 npx shadcn-vue@latest add switch
 
+import { Copy } from 'lucide-vue-next'
 import { useToast } from "@/components/ui/toast/use-toast";
 const { toast } = useToast();
-import errorCodes from "~/utils/codes";
 const { t } = useI18n()
 import { generateSeedPhrase, getMasterKeyFromSeed } from "~/utils/crypto";
 import * as bip32 from 'bip32';
+import { setKeyValue } from '~/utils/cookies';
 const loading = ref(false);
 
 
@@ -106,17 +86,8 @@ const generateSeed = () => {
 }
 
 const storeKey = () => {
-  // userSaveSeedDuration.value
-  const keyCookie = useCookie('key',{
-    maxAge: 60 * 60 * 24 * userSaveSeedDuration.value,
-    path: '/',
-    secure: true, // Only send cookie over HTTPS
-    httpOnly: false, // Allow JavaScript access to cookie
-    sameSite: 'strict', // Prevent CSRF attacks
-    signed: true, // Sign cookie to verify integrity
-  })
   const key : bip32.BIP32Interface = getMasterKeyFromSeed(seed.value.join(" "));
-  keyCookie.value = key;   
+  setKeyValue(userSaveSeedDuration,key as string);
   toast({
       title: t("success"),
       description: t("key_seed_saved"),
