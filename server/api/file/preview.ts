6b00ecc3ffcd268adb8fdc680f4d7827e64bfdfc
newtondotcom <asphalt9fr@gmail.com>
2024-07-05 16:55:06 +0200

    endpoint to get file preview url

---
+++ server/api/file/preview.ts
@@ -0,0 +1,12 @@
+import {
+  createPresignedUrlDownload,
+} from "~/server/data/s3";
+
+export default defineEventHandler(async (event) => {
+  const user_id = event.context.user_id;
+  const body = await readBody(event);
+  const name_s3 = body.name_s3;
+  // log file access
+  const url = await createPresignedUrlDownload(name_s3);
+  return { url};
+});
