0fe075a29cf3c4bfcd96e6f35bf87ab3472aa8f2
newtondotcom <asphalt9fr@gmail.com>
2024-07-05 16:23:45 +0200

    cookies managing in dedicated ts file

---
+++ utils/cookies.ts
@@ -0,0 +1,25 @@
+
+
+export function getKeyValue(){
+    const keyCookie = useCookie('key',{
+    path: '/',
+    secure: true, // Only send cookie over HTTPS
+    httpOnly: false, // Allow JavaScript access to cookie
+    sameSite: 'strict', // Prevent CSRF attacks
+    signed: true, // Sign cookie to verify integrity
+  })
+    return keyCookie.value;
+}
+
+//userSaveSeedDuration in days
+export function setKeyValue(userSaveSeedDuration : number,value : string){
+    const keyCookie = useCookie('key',{
+    maxAge: 60 * 60 * 24 * userSaveSeedDuration,
+    path: '/',
+    secure: true, // Only send cookie over HTTPS
+    httpOnly: false, // Allow JavaScript access to cookie
+    sameSite: 'strict', // Prevent CSRF attacks
+    signed: true, // Sign cookie to verify integrity
+  })
+  keyCookie.value = value;
+}
