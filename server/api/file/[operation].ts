89918d2083ba7cdf02e30b84890048d473a52ef9
newtondotcom <asphalt9fr@gmail.com>
2024-07-10 00:04:05 +0200

    grouping all endpoints to one

---
+++ server/api/file/[operation].ts
@@ -0,0 +1,72 @@
+import errorCodes from "~/utils/codes"
+import { createFile , fileExists ,replaceFile} from '~/server/data/files';
+import {createPresignedUrlDownload , createPresignedUrlUpload,
+  generateUniqueName} from '~/server/data/s3';
+
+export default defineEventHandler(async (event) => {
+  const operation = getRouterParam(event, 'operation');
+  switch (operation) {
+    case 'create':
+      return create(event);
+    case 'preview':
+      return preview(event);
+    case 'replace':
+      return replace(event);
+    case 'upload':
+      return upload(event);
+    default:
+      return errorCodes.method_not_allowed;
+  }
+
+});
+
+async function create(event) {
+  const user_id = event.context.user_id;
+  const body = await readBody(event);
+  const name = body.name;
+  const idParent = body.idParent;
+  const statut = body.statut;
+  const size = body.size;
+  const extension = body.extension;
+  const exists = await fileExists(name, extension, idParent, user_id);
+  if (exists) {
+    return errorCodes.file_already_exists;
+  }
+  const id = await createFile(name, extension, idParent, size, statut, user_id);
+  return id.id;
+};
+
+async function preview(event){
+  const user_id = event.context.user_id;
+  const body = await readBody(event);
+  const name_s3 = body.name_s3;
+  // log file access
+  const url = await createPresignedUrlDownload(name_s3);
+  return { url };
+}
+
+async function replace(event){
+  const user_id = event.context.user_id;
+  const body = await readBody(event);
+  const fileId = body.fileId;
+  const uname = generateUniqueName();
+  const urlUpload = await createPresignedUrlDownload(user_id, uname);
+  const id = await replaceFile(user_id, fileId, uname);
+  const idfinal = id.id;
+  return { idfinal, urlUpload };
+}
+
+async function upload(event){
+  const user_id = event.context.user_id;
+  const body = await readBody(event);
+  const name = body.name;
+  const idParent = body.idParent;
+  const statut = body.statut;
+  const size = body.size;
+  const extension = body.extension;
+  const id = await createFile(name, extension, idParent, size, statut, user_id);
+  const uname = generateUniqueName();
+  const { url, objectName } = await createPresignedUrlUpload(uname);
+  const idfinal = id.id;
+  return { idfinal, url, objectName };
+}
--- server/api/folder/create.ts
+++ server/api/folder/create.ts
@@ -1,4 +1,5 @@
-import { createFolder } from '~/server/data/files';
+import { createFolder, folderExists } from '~/server/data/files';
+import errorCodes from "~/utils/codes"
 
 export default defineEventHandler(async (event) => {
   const user_id = event.context.user_id;
@@ -6,6 +7,10 @@ export default defineEventHandler(async (event) => {
   const name = body.name;
   const idParent = body.idParent;
   const statut = body.statut;
+  const exists = await folderExists(name, idParent, user_id);
+  if (exists) {
+    return errorCodes.folder_already_exists;
+  }
   const id = await createFolder(name, idParent, statut, user_id);
   return id.id;
 });
