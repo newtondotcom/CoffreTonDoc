588153531932bd1d72526f31deb5a42a93b22282
newtondotcom <asphalt9fr@gmail.com>
2024-07-10 23:20:14 +0200

    attaching user_id to request

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,14 +1,15 @@
-import { getServerSession } from '#auth';
+import { getServerSession, getToken } from '#auth';
 
 export default eventHandler(async (event) => {
-    const session = await getServerSession(event);
-    const allowedRoutes: String[] = [''];
-    if (
-        event.node.req.url?.startsWith('auth/') ||
-        allowedRoutes.includes(!event.node.req.url)
-    ) {
-        throw { statusMessage: 'Unauthenticated', statusCode: 403 };
-    } else if (session) {
-        event.context.session = session;
-    }
+ const session = await getServerSession(event);
+ const allowedRoutes: String[] = [''];
+ if (
+  event.node.req.url?.startsWith('auth/') ||
+  allowedRoutes.includes(!event.node.req.url)
+ ) {
+  console.error('Unauthenticated route :', event.node.req.url);
+  throw { statusMessage: 'Unauthenticated', statusCode: 403 };
+ } else if (session) {
+  event.context.user_id = session.user.id;
+ }
 });
