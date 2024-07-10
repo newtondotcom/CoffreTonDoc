2204fa17f9f3e86d23f7bea464c835bb7a859517
newtondotcom <asphalt9fr@gmail.com>
2024-07-10 23:18:41 +0200

    prefill with test values

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -7,159 +7,139 @@ import errorCodes from '~/utils/codes';
 const { toast } = useToast();
 
 definePageMeta({
-    layout: false,
-    auth: {
-        unauthenticatedOnly: true,
-        navigateAuthenticatedTo: '/platform',
-    },
+ layout: false,
+ auth: {
+  unauthenticatedOnly: true,
+  navigateAuthenticatedTo: '/platform',
+ },
 });
 
-const username = ref('');
-const password = ref('');
+const username = ref('testrandom2@gmail.com');
+const password = ref('test');
 const loading = ref(false);
 
 const value = ref<String[]>([]);
 const totpCode = ref(0);
 const handleComplete = (e: String[]) => {
-    totpCode.value = e.join('');
-    login();
+ totpCode.value = e.join('');
+ login();
 };
 
 const login = async () => {
-    loading.value = true;
-    if (username.value == '' || password.value == '' || totpCode.value == 0) {
-        toast({
-            title: t('error'),
-            description: t('all_fields'),
-            variant: 'destructive',
-        });
-        loading.value = false;
-        return;
-    }
-    const response = await signIn('credentials', {
-        redirect: false,
-        username: username.value.trim(),
-        password: password.value.trim(),
-        totpCode: totpCode.value,
-    });
-    switch(response){
-        case errorCodes.incorrect_username || errorCodes.incorrect_password : {
-        toast({
-            title: t('error'),
-            description: t('wrong_credentials'),
-            variant: 'destructive',
-        });
-        loading.value = false;
-        break;
-        }
-        case errorCodes.incorrect_two_factor_code : {
-        toast({
-            title: t('error'),
-            description: t('wrong_totp'),
-            variant: 'destructive',
-        });
-        loading.value = false;
-        break;
-        }
-        default : {
-            navigateTo('/platform');
-            break;
-        }
-        
-        }
+ loading.value = true;
+ if (username.value == '' || password.value == '' || totpCode.value == 0) {
+  toast({
+   title: t('error'),
+   description: t('all_fields'),
+   variant: 'destructive',
+  });
+  loading.value = false;
+  return;
+ }
+ const response = await signIn('credentials', {
+  redirect: false,
+  username: username.value.trim(),
+  password: password.value.trim(),
+  totpCode: totpCode.value,
+ });
+ switch (response) {
+  case errorCodes.incorrect_username || errorCodes.incorrect_password: {
+   toast({
+    title: t('error'),
+    description: t('wrong_credentials'),
+    variant: 'destructive',
+   });
+   loading.value = false;
+   break;
+  }
+  case errorCodes.incorrect_two_factor_code: {
+   toast({
+    title: t('error'),
+    description: t('wrong_totp'),
+    variant: 'destructive',
+   });
+   loading.value = false;
+   break;
+  }
+  default: {
+   navigateTo('/platform');
+   break;
+  }
+ }
 };
 </script>
 
 <template>
-    <div class="flex h-screen items-center justify-center">
-        <Card class="mx-auto max-w-sm">
-            <CardHeader>
-                <CardTitle class="text-2xl"> {{ $t('connect_us') }} </CardTitle>
-                <CardDescription>
-                    {{ $t('login_description') }}
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <div class="grid gap-4">
-                    <div class="grid gap-2">
-                        <Label class="my-2" for="email">{{
-                            $t('email')
-                        }}</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            placeholder="m@example.com"
-                            required
-                            v-model="username"
-                        />
-                    </div>
-                    <div class="grid gap-2">
-                        <div class="flex items-center">
-                            <Label class="my-2" for="password">{{
-                                $t('password')
-                            }}</Label>
-                            <NuxtLink
-                                to="/auth/reset"
-                                class="ml-auto inline-block text-sm underline"
-                            >
-                                Forgot your password?
-                            </NuxtLink>
-                        </div>
-                        <Input
-                            id="password"
-                            type="password"
-                            required
-                            v-model="password"
-                        />
-                    </div>
-                    <div class="grid gap-2">
-                        <Label class="my-2" for="email">{{
-                            $t('twofa_code')
-                        }}</Label>
-                        <PinInput
-                            id="pin-input"
-                            v-model="value"
-                            placeholder="○"
-                            @complete="handleComplete"
-                        >
-                            <PinInputGroup>
-                                <PinInputInput
-                                    v-for="(id, index) in 6"
-                                    :key="id"
-                                    :index="index"
-                                />
-                            </PinInputGroup>
-                        </PinInput>
-                    </div>
+ <div class="flex h-screen items-center justify-center">
+  <Card class="mx-auto max-w-sm">
+   <CardHeader>
+    <CardTitle class="text-2xl"> {{ $t('connect_us') }} </CardTitle>
+    <CardDescription>
+     {{ $t('login_description') }}
+    </CardDescription>
+   </CardHeader>
+   <CardContent>
+    <div class="grid gap-4">
+     <div class="grid gap-2">
+      <Label class="my-2" for="email">{{ $t('email') }}</Label>
+      <Input
+       id="email"
+       type="email"
+       placeholder="m@example.com"
+       required
+       v-model="username"
+      />
+     </div>
+     <div class="grid gap-2">
+      <div class="flex items-center">
+       <Label class="my-2" for="password">{{ $t('password') }}</Label>
+       <NuxtLink
+        to="/auth/reset"
+        class="ml-auto inline-block text-sm underline"
+       >
+        Forgot your password?
+       </NuxtLink>
+      </div>
+      <Input id="password" type="password" required v-model="password" />
+     </div>
+     <div class="grid gap-2">
+      <Label class="my-2" for="email">{{ $t('twofa_code') }}</Label>
+      <PinInput
+       id="pin-input"
+       v-model="value"
+       placeholder="○"
+       @complete="handleComplete"
+      >
+       <PinInputGroup>
+        <PinInputInput v-for="(id, index) in 6" :key="id" :index="index" />
+       </PinInputGroup>
+      </PinInput>
+     </div>
 
-                    <ButtonLoading
-                        :loading
-                        :text="t('login')"
-                        :execute="login"
-                    />
-                </div>
-                <div class="mt-4 text-center text-sm">
-                    {{ $t('allow_using') }}
-                    <NuxtLink class="text-gray-500 underline" to="/tos">{{
-                        $t('tos')
-                    }}</NuxtLink>
-                    and
-                    <NuxtLink to="/privacy" class="text-gray-500 underline">{{
-                        $t('privacy')
-                    }}</NuxtLink>
-                    <br />
-
-                    <span class="text-xs">
-                        {{ $t('data_collect') }}
-                    </span>
-                </div>
-                <div class="mt-4 text-center text-sm">
-                    {{ $t('no_account') }}
-                    <NuxtLink to="/auth/register" class="underline">
-                        {{ $t('register') }}
-                    </NuxtLink>
-                </div>
-            </CardContent>
-        </Card>
+     <ButtonLoading :loading :text="t('login')" :execute="login" />
     </div>
+    <div class="mt-4 text-center text-sm">
+     {{ $t('allow_using') }}
+     <NuxtLink class="text-gray-500 underline" to="/tos">{{
+      $t('tos')
+     }}</NuxtLink>
+     and
+     <NuxtLink to="/privacy" class="text-gray-500 underline">{{
+      $t('privacy')
+     }}</NuxtLink>
+     <br />
+
+     <span class="text-xs">
+      {{ $t('data_collect') }}
+     </span>
+    </div>
+    <div class="mt-4 text-center text-sm">
+     {{ $t('no_account') }}
+     <NuxtLink to="/auth/register" class="underline">
+      {{ $t('register') }}
+     </NuxtLink>
+    </div>
+   </CardContent>
+  </Card>
+ </div>
 </template>
