96f6dc8ec15eadfbeb28f01409fe94a7ed8471df
newtondotcom <asphalt9fr@gmail.com>
2024-08-17 16:37:14 -0700

    feat : function for hash and not hash address

--- utils/cookie.ts
+++ utils/cookie.ts
@@ -25,5 +25,43 @@ export function setAddValue(value: string) {
         sameSite: 'strict', // Prevent CSRF attacks
         signed: true, // Sign cookie to verify integrity
     });
+    if (value === '') {
+        keyCookie.value = undefined;
+        return;
+    }
+    keyCookie.value = value;
+}
+
+/**
+ * Retrieves the value of the 'hash-add' cookie.
+ * @returns {string | undefined} The value of the 'hash-add' cookie if it exists, otherwise undefined.
+ */
+export function getHashAddValue() {
+    const keyCookie = useCookie('hash-add', {
+        path: '/',
+        secure: true, // Only send cookie over HTTPS
+        httpOnly: false, // Allow JavaScript access to cookie
+        sameSite: 'strict', // Prevent CSRF attacks
+        signed: true, // Sign cookie to verify integrity
+    });
+    return keyCookie.value;
+}
+
+/**
+ * Sets the value of the 'hash-add' cookie.
+ * @param {string} value - The value to set for the 'hash-add' cookie.
+ */
+export function setHashAddValue(value: string) {
+    const keyCookie = useCookie('hash-add', {
+        path: '/',
+        secure: true, // Only send cookie over HTTPS
+        httpOnly: false, // Allow JavaScript access to cookie
+        sameSite: 'strict', // Prevent CSRF attacks
+        signed: true, // Sign cookie to verify integrity
+    });
+    if (value === '') {
+        keyCookie.value = undefined;
+        return;
+    }
     keyCookie.value = value;
 }
