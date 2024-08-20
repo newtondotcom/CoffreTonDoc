f7102bd74da8ac8f87e55b592d682d918a66a31c
newtondotcom <asphalt9fr@gmail.com>
2024-08-19 21:25:56 -0700

    feat : delete file from s3

--- server/data/s3.ts
+++ server/data/s3.ts
@@ -1,7 +1,7 @@
 import { Client } from 'minio';
 import prisma from './prisma';
-
-const name_s3_vault = 'vault';
+import constants from '~/lib/constants';
+import { setFileRecordDeleted } from './files';
 
 export function generateUniqueName() {
     const date = new Date();
@@ -14,7 +14,7 @@ export function generateUniqueName() {
 export async function createPresignedUrlUpload(uname: string) {
     const config = await prisma.s3.findUnique({
         where: {
-            name: name_s3_vault,
+            name: constants.name_s3_vault,
         },
     });
 
@@ -38,9 +38,10 @@ export async function createPresignedUrlUpload(uname: string) {
 }
 
 export async function createPresignedUrlDownload(objectName: any) {
+    console.log('objectName', objectName);
     const config = await prisma.s3.findUnique({
         where: {
-            name: name_s3_vault,
+            name: constants.name_s3_vault,
         },
     });
     const MINIO_ENDPOINT = config.endpoint;
@@ -60,6 +61,31 @@ export async function createPresignedUrlDownload(objectName: any) {
     const expiryInSeconds = 3600;
     const url = await minioClient.presignedGetObject(bucketName, objectName, expiryInSeconds);
     return url;
+}
+
+export async function deleteFile(name: string) {
+    const config = await prisma.s3.findUnique({
+        where: {
+            name: constants.name_s3_vault,
+        },
+    });
+    const MINIO_ENDPOINT = config.endpoint;
+    const MINIO_PORT = config.port;
+    const MINIO_ACCESS_KEY = config.access_key;
+    const MINIO_SECRET_KEY = config.secret_key;
+    const MINIO_SSL = config.ssl;
+    const bucketName = config.bucket;
+
+    const minioClient = new Client({
+        endPoint: MINIO_ENDPOINT,
+        port: MINIO_PORT,
+        useSSL: MINIO_SSL,
+        accessKey: MINIO_ACCESS_KEY,
+        secretKey: MINIO_SECRET_KEY,
+    });
+
+    await minioClient.removeObject(bucketName, name);
+    await setFileRecordDeleted(name);
 }
 
 export async function insertS3Server(
