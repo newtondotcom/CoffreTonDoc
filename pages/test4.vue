0b348b560e82170d3a86068e597ccb9b200acb6a
newtondotcom <asphalt9fr@gmail.com>
2024-09-18 17:51:33 +0200

    fix : final version for minio issue

--- pages/test4.vue
+++ pages/test4.vue
@@ -8,16 +8,6 @@
     const req = await $fetch('/api/test');
     const link = req.link;
     const download = req.download;
-    const length = ref(0);
-    const encryptedData = ref(new Uint8Array(0));
-
-    const arraysEqual = (a: Uint8Array, b: Uint8Array) => {
-        if (a.length !== b.length) return false;
-        for (let i = 0; i < a.length; i++) {
-            if (a[i] !== b[i]) return false;
-        }
-        return true;
-    };
 
     function readFile(file: File): Promise<Uint8Array> {
         return new Promise((resolve, reject) => {
@@ -28,32 +18,42 @@
         });
     }
 
-    const up = async (event) => {
-        const file = event.target.files[0];
-        const ethAddress = getAddValue();
-        const data = await readFile(file);
-        console.log('File read:', data.length); // 1500066
-        /*
-        const key = await deriveKeyFromEthAddress(ethAddress);
-        encryptedData.value = await encryptFile(key, data);
-        console.log('Encrypted data:', encryptedData.value.length);
-        length.value = encryptedData.value.length;
-        */
-        await uploadFile(data, link);
-        console.log('File uploaded');
-        await dl();
+    const arraysEqual = (a: Uint8Array, b: Uint8Array) => {
+        if (a.length !== b.length) return false;
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) return false;
+        }
+        return true;
     };
 
-    const dl = async () => {
-        const ethAddress = getAddValue();
-        const key = await deriveKeyFromEthAddress(ethAddress);
+    async function uploadFile(dataToUpload: Uint8Array, url: string) {
+        try {
+            const formData = new FormData();
+            formData.append('file', dataToUpload);
+            await fetch(url, {
+                method: 'PUT',
+                body: formData,
+            });
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    }
+
+    const up = async (event) => {
+        const file = event.target.files[0];
+        const data = await readFile(file);
+        console.log('File read:', data.length); // 1500066
+
+        // Upload the file
+        await uploadFile(data, link);
+        console.log('File uploaded');
+
+        // Download the file
         const fileFetch = await fetch(download);
-        const data: Uint8Array = new Uint8Array(await fileFetch.arrayBuffer());
-        console.log('Downloaded data:', data.length); // 5300707
-        /*
-        //console.log(arraysEqual(local, encryptedData.value));
-        const decryptedData = await decryptFile(key, encryptedData);
-        console.log('Decrypted data:', decryptedData.length);
-        */
+        const data2: Uint8Array = new Uint8Array(await fileFetch.arrayBuffer());
+        console.log('Downloaded data:', data2.length); // 5300707 or 5300703
+
+        const cropped = data2.slice(0, data.length);
+        console.log('Arrays equal:', arraysEqual(data, cropped)); // false
     };
 </script>
