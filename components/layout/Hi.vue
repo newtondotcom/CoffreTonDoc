54e41ac176aae078cfa0a2ef1eea0608ad7c47fe
newtondotcom <asphalt9fr@gmail.com>
2024-09-26 11:19:08 +0200

    fix : changing order

--- components/layout/Hi.vue
+++ components/layout/Hi.vue
@@ -1,11 +1,5 @@
 <template>
     <div class="my-4 flex flex-col items-center justify-center gap-4 px-4">
-        <div class="flex flex-row items-center justify-center gap-4">
-            <div class="grid gap-1">
-                <p class="text-sm text-muted-foreground">{{ $t('logged_in') }}</p>
-                <p class="truncate text-sm font-medium leading-none">{{ auth }}</p>
-            </div>
-        </div>
         <div v-if="status === 'authenticated'" class="flex flex-row items-center">
             <div class="mr-2 h-3 w-3 rounded-full bg-green-500"></div>
             {{ $t('authenticated') }}
@@ -13,6 +7,12 @@
         <div v-else class="flex flex-row items-center">
             <div class="mr-2 h-3 w-3 rounded-full bg-red-500"></div>
             {{ $t('unauthenticated') }}
+        </div>
+        <div class="flex flex-row items-center justify-center gap-4">
+            <div class="grid gap-1">
+                <p class="text-sm text-muted-foreground">{{ $t('logged_in') }}</p>
+                <p class="truncate text-sm font-medium leading-none">{{ auth }}</p>
+            </div>
         </div>
         <Button class="w-[70%]" variant="destructive" @click="logout">Log out</Button>
     </div>
